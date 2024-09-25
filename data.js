910731bdab4e3dc7eeacec0bb5ae4ce6a2b77dd5
sharad8286 <125875253+sharad8286@users.noreply.github.com>
2024-09-25 10:59:19 +0530

    Add files via upload

---
+++ data.js
@@ -0,0 +1,4 @@
+var username,password;
+
+username=['ram@123','mohan@123'];
+password=[123,124];
---
+++ security.js
@@ -0,0 +1,6 @@
+          //Protected Page
+          var username = localStorage.getItem("user");
+          if(!username){
+           
+            window.location.href="Login.html";
+        }
